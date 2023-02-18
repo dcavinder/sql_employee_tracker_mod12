f91f2b8d599fdea2cb412404cbbd392326109350
Danielle Cavinder <daniellecavinder@gmail.com>
2023-02-18 08:56:52 -0700

    fixed require issue and connection to db

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const db = mysql.createConnection('mysql://root:rootroot@localhost:3306/employee_tracker_db')
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_tracker_db'
+})
+//const db = mysql.createConnection('mysql://root:root@localhost:3306/employee_tracker_db')
 
 
-inquirer.createPromptModule([{
+inquirer.prompt([{
     message: 'Please select the action you would like to perform',
     type: 'list',
     choices: ['Add a Department', 'Add a Role', 'Add an Employee', 'View all Departments', 'View all Roles',
