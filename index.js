7f792ef1ceb477f0ad59d3d6053c3b4b9535083a
Danielle Cavinder <daniellecavinder@gmail.com>
2023-02-18 20:31:46 -0700

    added switch case and file structure for functions to add and view items

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -8,16 +9,38 @@ const db = mysql.createConnection({
     password: '',
     database: 'employee_tracker_db'
 })
-//const db = mysql.createConnection('mysql://root:root@localhost:3306/employee_tracker_db')
-
 
 inquirer.prompt([{
     message: 'Please select the action you would like to perform',
     type: 'list',
     choices: ['Add a Department', 'Add a Role', 'Add an Employee', 'View all Departments', 'View all Roles',
     'View all Employees', 'Update an Employee Role'],
-    name: 'init'
+    name: 'action'
 }])
 .then(init => {
 console.log(init)
+
+switch(init.action) {
+    case 'Add a Department':
+        addDept()
+        break
+    case 'Add a Role':
+        addRole()
+        break
+    case 'Add an Employee': 
+        addEmpl()
+        break
+    case 'View all Departments':
+        viewDepts()
+        break
+    case 'View all Roles':
+        viewRoles()
+        break
+    case 'View all Employees':
+        viewEmployees()
+        break
+    case 'Update an Employee Role':
+        updateEmplRole()
+        break
+}
 })
